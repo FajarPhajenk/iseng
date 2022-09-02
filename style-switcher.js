947b92bc93b6ca0588fa077b8dc869a2b086ae80
FajarPhajenk <Fajarrcf09@gmail.com>
2022-09-02 22:24:25 +0700

    rubahan

--- style-switcher.js
+++ style-switcher.js
@@ -3,3 +3,24 @@ const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 });
+// hide style switch
+window.addEventListener("scroll"),
+    () => {
+        if (document.querySelector(".style-switcher").classList.contains("open")) {
+            document.querySelector(".style-switcher").classList.remove("open");
+        }
+    };
+
+// theme color
+
+const alternateStyles = document.querySelector(".alternate-styles");
+
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAtribbute("title")) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
